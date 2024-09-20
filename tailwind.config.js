ff3ccdfd11067b1255c6e6cb16a4d5fee02d6d21
Marius Odediran <themariusodediran@gmail.com>
2024-09-20 14:23:51 +0100

    Buliding a To-Do List web

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      Roboto: ['Roboto', 'sans-serif'],
+      JetBrainsMono: ['JetBrains Mono', 'monospace'],
+      Lobster: ['Lobster', 'cursive']
+    }
+  },
+  plugins: [],
+}
+
